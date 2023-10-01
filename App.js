6124769d83d84b5813a80ffb190efe8fb23fccfa
DucLam <lam21240408@gmail.com>
2023-10-01 19:33:10 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import  ManHinh1  from "./manhinh/manHinh1";
+import  ManHinh2  from "./manhinh/manHinh2";
+import  ManHinh3  from "./manhinh/manHinh3";
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // <ManHinh1/>
+    // <ManHinh2/>
+    <ManHinh3/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
